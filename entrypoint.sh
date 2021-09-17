6e1f2e03a2a28812c8fe978ba5fbca31cf6ed7ed
Shogo Sawai <shogo.sawai@gmail.com>
2021-09-17 15:10:41 +0900

    support host_key mount

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,11 +6,17 @@ set -eu  # <= 0以外が返るものがあったら止まる, 未定義の変数
 mkdir ~/.ssh
 
 sed -i 's/#HostKey \/etc\/ssh\/ssh_host_rsa_key/HostKey \/etc\/ssh\/ssh_host_rsa_key/' /etc/ssh/sshd_config
-chown root:root /etc/ssh/ssh_host_rsa_key
-chmod 0400 /etc/ssh/ssh_host_rsa_key
+if [ ${SSH_HOST_RSA_KEY:-""} != "" ]; then
+    cp ${SSH_HOST_RSA_KEY} /etc/ssh/ssh_host_rsa_key
+    chown root:root /etc/ssh/ssh_host_rsa_key
+    chmod 0400 /etc/ssh/ssh_host_rsa_key
+fi
 
-chown root:root /etc/ssh/ssh_host_rsa_key.pub
-chmod 0444 /etc/ssh/ssh_host_rsa_key.pub
+if [ ${SSH_HOST_RSA_KEY_PUB:-""} != "" ]; then
+    cp ${SSH_HOST_RSA_KEY_PUB} /etc/ssh/ssh_host_rsa_key.pub
+    chown root:root /etc/ssh/ssh_host_rsa_key.pub
+    chmod 0444 /etc/ssh/ssh_host_rsa_key.pub
+fi
 
 echo ${AUTHORIZED_KEYS} > ~/.ssh/authorized_keys
 chmod 0400 ~/.ssh/authorized_keys
