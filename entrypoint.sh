c4d17ffe25828149be0fd11fc090fcaba2595a29
Shogo Sawai <shogo.sawai@gmail.com>
2021-09-17 14:32:31 +0900

    set host_key

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,14 @@
 set -eu  # <= 0以外が返るものがあったら止まる, 未定義の変数を使おうとしたときに打ち止め
 
 mkdir ~/.ssh
+
+sed -i 's/#HostKey \/etc\/ssh\/ssh_host_rsa_key/HostKey \/etc\/ssh\/ssh_host_rsa_key/' /etc/ssh/sshd_config
+chown root:root /etc/ssh/ssh_host_rsa_key
+chmod 0400 /etc/ssh/ssh_host_rsa_key
+
+chown root:root /etc/ssh/ssh_host_rsa_key.pub
+chmod 0444 /etc/ssh/ssh_host_rsa_key.pub
+
 echo ${AUTHORIZED_KEYS} > ~/.ssh/authorized_keys
 chmod 0400 ~/.ssh/authorized_keys
 
--- readme.md
+++ readme.md
@@ -9,3 +9,6 @@ docker run --rm -t -i -p 22222:22 -e AUTHORIZED_KEYS="`cat ~/.ssh/id_rsa.pub`" t
 ```
 ssh -o  StrictHostKeyChecking=no  root@localhost -p 22222
 ```
+
+
+`ssh-keygen -q -N "" -t rsa -b 4096 -f ssh_host_rsa_key`
